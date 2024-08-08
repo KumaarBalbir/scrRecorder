527691a3e56431fbab0591eedac6aa72d15ab84d
balbir <prasadbalbir1056@gmail.com>
2024-08-08 16:46:22 +0530

    feat: render vid stream preview

--- src/render.js
+++ src/render.js
@@ -21,3 +21,36 @@ async function getVideoSources() {
 
   videoOptionsMenu.popup();
 }
+
+// once a screen is selected, it should be provided in the video element
+let mediaRecorder; // MediaRecorder instance to capture footage
+const recordedChunks = [];
+
+// change the video source window to record
+async function selectSource(source) {
+  videoSelectBtn.innerText = source.name;
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: "desktop",
+        chromeMediaSourceId: source.id,
+      },
+    },
+  };
+
+  // Create a Stream
+  const stream = await navigator.mediumDevices.getUserMedia(constraints);
+
+  // Preview the source in a video element
+  videoElement.srcObject = stream;
+  videoElement.play();
+
+  // create the media recorder
+  const options = { mimeType: "video/webm; codecs=vp9" };
+  mediaRecorder = new MediaRecorder(stream, options);
+
+  // Register Event Handlers
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop;
+}
